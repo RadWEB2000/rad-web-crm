57f33440d2597bf857d54a1e6bf9db3edc896401
RadoslawAdamczyk2000 <greatpolandweb@gmail.com>
2023-11-04 08:30:30 +0100

    check changes

--- src/app/(dashboard)/dashboard/client/page.tsx
+++ src/app/(dashboard)/dashboard/client/page.tsx
@@ -1,24 +1,24 @@
-import GoogleSearchConsole from 'components/Views/DashboardView/ClientView/GoogleSearchConsole/GoogleSearchConsole';
-import getGoogleSearchConsoleData from 'configs/gsc';
-import getGooglePageSpeedData from 'configs/pagespeed';
-import PageSpeedInsights from 'components/Views/DashboardView/ClientView/PageSpeedInsights/PageSpeedInsights';
-import styles from 'app/(dashboard)/dashboard/client/_style/Client.module.scss';
-import Details from 'components/Views/DashboardView/ClientView/Details';
-import { exampleClient } from 'data/exampleClient';
+import GoogleSearchConsole from "components/Views/DashboardView/ClientView/GoogleSearchConsole/GoogleSearchConsole";
+import getGoogleSearchConsoleData from "configs/gsc";
+import getGooglePageSpeedData from "configs/pagespeed";
+import PageSpeedInsights from "components/Views/DashboardView/ClientView/PageSpeedInsights/PageSpeedInsights";
+import styles from "app/(dashboard)/dashboard/client/_style/Client.module.scss";
+import Details from "components/Views/DashboardView/ClientView/Details";
+import { exampleClient } from "data/exampleClient";
 
 export default async function ClientPage() {
-	const site: string = 'https://rad-web.pl';
+	const site: string = "https://rad-web.pl";
 	const site2: string = site;
 
 	const gsc = {
-		date: await getGoogleSearchConsoleData('date', site),
-		page: await getGoogleSearchConsoleData('page', site),
-		query: await getGoogleSearchConsoleData('query', site),
+		date: await getGoogleSearchConsoleData("date", site),
+		page: await getGoogleSearchConsoleData("page", site),
+		query: await getGoogleSearchConsoleData("query", site),
 	};
 
 	const psi = {
-		desktop: await getGooglePageSpeedData(site2, 'desktop'),
-		mobile: await getGooglePageSpeedData(site2, 'mobile'),
+		desktop: await getGooglePageSpeedData(site2, "desktop"),
+		mobile: await getGooglePageSpeedData(site2, "mobile"),
 	};
 
 	const psiData = {
@@ -26,15 +26,14 @@ export default async function ClientPage() {
 		desktop: psi.desktop.lighthouseResult.categories,
 		preview: {
 			alt: psi.desktop.kind,
-			src: psi.desktop.lighthouseResult.audits['final-screenshot'].details
-				.data,
+			src: psi.desktop.lighthouseResult.audits["final-screenshot"].details.data,
 			title: psi.desktop.kind,
 		},
 	};
 
 	console.log(psi);
 	return (
-		<div className={styles.wrapper}>
+		<div className={styles.wrapper} style={{ backgroundColor: "#fff" }}>
 			<div className={styles.details}>
 				<Details
 					domain={{
@@ -43,17 +42,18 @@ export default async function ClientPage() {
 					}}
 					managment={{
 						cooperation: {
-							start: 'Tue Feb 21 2023 15:29:03 GMT+0100 (Central European Standard Time)',
+							start:
+								"Tue Feb 21 2023 15:29:03 GMT+0100 (Central European Standard Time)",
 							end: `Sun Aug 11 2024 03:41:07 GMT+0200 (Central European Summer Time)`,
 						},
 						specialists: [
 							{
-								label: 'Klaudia Paluch',
-								uri: '#',
+								label: "Klaudia Paluch",
+								uri: "#",
 							},
 							{
-								label: 'Radosław Adamczyk',
-								uri: '#',
+								label: "Radosław Adamczyk",
+								uri: "#",
 							},
 						],
 					}}
@@ -71,21 +71,16 @@ export default async function ClientPage() {
 					preview={psiData.preview}
 					statistics={{
 						desktop: {
-							accessibility:
-								psiData.desktop.accessibility.score * 100,
-							performance:
-								psiData.desktop.performance.score * 100,
+							accessibility: psiData.desktop.accessibility.score * 100,
+							performance: psiData.desktop.performance.score * 100,
 							seo: psiData.desktop.seo.score * 100,
-							practices:
-								psiData.desktop['best-practices'].score * 100,
+							practices: psiData.desktop["best-practices"].score * 100,
 						},
 						mobile: {
-							accessibility:
-								psiData.mobile.accessibility.score * 100,
+							accessibility: psiData.mobile.accessibility.score * 100,
 							performance: psiData.mobile.performance.score * 100,
 							seo: psiData.mobile.seo.score * 100,
-							practices:
-								psiData.mobile['best-practices'].score * 100,
+							practices: psiData.mobile["best-practices"].score * 100,
 						},
 					}}
 				/>
